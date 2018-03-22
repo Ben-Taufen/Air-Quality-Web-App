aeaad9553fbba7cafdbed433fad8e3ab22cb8d1e
teub0005 <31710545+teub0005@users.noreply.github.com>
2018-03-21 20:05:58 -0500

    Add files via upload

---
+++ airQualityApp.js
@@ -0,0 +1,13 @@
+var airQualityApp = angular.module('airQualityApp',[]);
+
+airQualityApp.controller('tableController',function($scope, $http){
+	//
+	$http.get("https://api.openaq.org/v1/measurements").then(
+		function(response)
+		{
+			$scope.tableData = response.data.results;
+			
+		}
+	);
+	
+});
